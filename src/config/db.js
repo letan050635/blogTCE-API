b832f77c1aebeb8e71e854281424ecdc276bd21c
letan050635 <letan050635@gmail.com>
2025-05-12 14:06:41 +0700

    optimize dashboard

--- src/config/db.js
+++ src/config/db.js
@@ -9,7 +9,8 @@ const pool = mysql.createPool({
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  charset: 'utf8mb4'
 });
 
 // Kiểm tra kết nối
